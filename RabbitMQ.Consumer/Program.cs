4ca90dc19044f92a32418bd8189d9490e09f9438
Fatma <fmutlu3002@gmail.com>
2023-04-28 14:30:01 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿
+//CONSUMER
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://odsbstrb:WqzIHX3r_dzHwhJuO6uDjMa2kasPZVnQ@cow.rmq2.cloudamqp.com/odsbstrb");
+
+//bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//queue oluşturma
+channel.QueueDeclare("example-rabbit", exclusive: false);
+
+//queue'dan mesaj okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume("example-rabbit", false, consumer);
+
+consumer.Received += (sender, e) =>
+{
+    //kuyruğa gelen mesajın işlendiği yer
+    //e.Body: kuyruktaki mesajın bütünsel verisi
+    //e.Body.Span veya e.Body.ToArray(): kuyruktaki mesajın byte verisi
+
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
+
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//PUBLISHER
+
+using RabbitMQ.Client;
+using System.Text;
+
+//bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://odsbstrb:WqzIHX3r_dzHwhJuO6uDjMa2kasPZVnQ@cow.rmq2.cloudamqp.com/odsbstrb");
+
+//bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel= connection.CreateModel();
+
+//queue oluşturma
+//rabbitMQ kuyruğa atacağı mesajları byte türünden kabul eder.
+channel.QueueDeclare(queue:"example-rabbit", exclusive:false);
+
+byte[] message = Encoding.UTF8.GetBytes("RabbitMQ");
+channel.BasicPublish(exchange:"", routingKey:"example-rabbit", body:message);
+
+Console.Read();
+
